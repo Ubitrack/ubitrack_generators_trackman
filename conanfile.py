85a307d3f2a33d55ce3e8493fde4320e2c08d77b
Ulrich Eck <ulrich.eck@tum.de>
2020-03-06 15:46:40 +0100

    adapted to changes in conan 1.22

--- conanfile.py
+++ conanfile.py
@@ -1,32 +1,53 @@
 from conans.client.generators.virtualrunenv import VirtualRunEnvGenerator
+from conans.client.run_environment import RunEnvironment
 from conans import ConanFile, CMake
 from conans.tools import os_info
 import os
 
+import textwrap
+from jinja2 import Template
+
+
+
+sh_trackman_script_tpl = Template(textwrap.dedent("""\
+    #!/usr/bin/env sh
+    {%- for it in modified_vars %}
+    export CONAN_OLD_{{it}}="${{it}}"
+    {%- endfor %}
+    while read -r line; do
+        LINE="$(eval echo $line)";
+        export "$LINE";
+    done < "{{ environment_file }}"
+    export CONAN_OLD_PS1="$PS1"
+    export PS1="({{venv_name}}) $PS1"
+    java -jar {{jar_path}}
+"""))
+
+cmd_trackman_script_tpl = Template(textwrap.dedent("""\
+    @echo off
+    
+    {%- for it in modified_vars %}
+    SET "CONAN_OLD_{{it}}=%{{it}}%"
+    {%- endfor %}
+    
+    FOR /F "usebackq tokens=1,* delims==" %%i IN ("{{ environment_file }}") DO (
+        CALL SET "%%i=%%j"
+    )
+    
+    SET "CONAN_OLD_PROMPT=%PROMPT%"
+    SET "PROMPT=({{venv_name}}) %PROMPT%"
+    java -jar {{jar_path}}
+"""))
+
 class ubitrack_virtualenv_generator(VirtualRunEnvGenerator):
+
+    venv_name = "ubitrackrunenv"
+    suffix    = "_utrun"
+
 
     def __init__(self, conanfile):
         super(ubitrack_virtualenv_generator, self).__init__(conanfile)
-        self.venv_name = "ubitrackrunenv"
 
-    def ubitrack_env_items(self):
-        items = {}
-        if os_info.is_windows and not os_info.is_posix:
-            items['UBITRACK_COMPONENTS_PATH'] = os.path.join(self.output_path, "bin", "ubitrack")
-        else:
-            items['UBITRACK_COMPONENTS_PATH'] = os.path.join(self.output_path, "lib", "ubitrack")
-
-        deps_env_vars = self.conanfile.deps_env_info.vars
-        for key, v in deps_env_vars.items():
-            if key == "TRACKMAN_BIN_PATH":
-              items["CLASSPATH"] = v[0]
-            # else:
-            #     if isinstance(v, list):
-            #         v = ":".join(v)
-            #     print("Export: %s=%s" % (key, v))
-            #     items[key] = v
-
-        return items.items()
 
     def trackman_config_items(self):
         items = {}
@@ -37,49 +58,6 @@ class ubitrack_virtualenv_generator(VirtualRunEnvGenerator):
         items["PatternTemplateDirectory"] = os.path.join(self.output_path, "share", "Ubitrack", "utql")
         items["UbitrackLibraryDirectory"] = os.path.join(self.output_path, "lib")
         return items.items()
-
-    def _sh_lines(self):
-        activate_lines, deactivate_lines, envfile = super(ubitrack_virtualenv_generator, self)._sh_lines()
-
-        variables = self.ubitrack_env_items()
-        
-        for name, activate, deactivate in self._format_values("sh", variables):
-            activate_lines.append("%s=%s" % (name, activate))
-            activate_lines.append("export %s" % name)
-            if deactivate == '""':
-                deactivate_lines.append("unset %s" % name)
-            else:
-                deactivate_lines.append("%s=%s" % (name, deactivate))
-                deactivate_lines.append("export %s" % name)            
-        activate_lines.append('')
-        deactivate_lines.append('')
-
-        return activate_lines, deactivate_lines, envfile
-
-    def _cmd_lines(self):
-        activate_lines, deactivate_lines, envfile = super(ubitrack_virtualenv_generator, self)._cmd_lines()
-
-        variables = self.ubitrack_env_items()
-
-        for name, activate, deactivate in self._format_values("cmd", variables):
-            activate_lines.append("SET %s=%s" % (name, activate))
-            deactivate_lines.append("SET %s=%s" % (name, deactivate))
-        activate_lines.append('')
-        deactivate_lines.append('')
-
-        return activate_lines, deactivate_lines, envfile
-
-    def _ps1_lines(self):
-        activate_lines, deactivate_lines, envfile = super(ubitrack_virtualenv_generator, self)._ps1_lines()
-
-        variables = self.ubitrack_env_items()
-
-        for name, activate, deactivate in self._format_values("ps1", variables):
-            activate_lines.append('$env:%s = %s' % (name,activate))
-            deactivate_lines.append('$env:%s = %s' % (name,deactivate))
-        activate_lines.append('')
-
-        return activate_lines, deactivate_lines, envfile
 
     def _trackman_config_lines(self):
         config_lines = []
@@ -93,6 +71,7 @@ class ubitrack_virtualenv_generator(VirtualRunEnvGenerator):
 
     def _add_trackman_files(self, result):
         deps_env_vars = self.conanfile.deps_env_info.vars
+        
         if "TRACKMAN_BIN_PATH" in deps_env_vars:
             trackman_items = {}
             trackman_items["CLASSPATH"] = deps_env_vars["TRACKMAN_LIB_PATH"][0]
@@ -103,30 +82,45 @@ class ubitrack_virtualenv_generator(VirtualRunEnvGenerator):
 
 
             if os_info.is_windows and not os_info.is_posix:
-                script_lines, _, _ = self._cmd_lines()
-                for name, activate, deactivate in self._format_values("cmd", trackman_items.items()):
-                    script_lines.append("SET %s=%s" % (name, activate))
-                script_lines.append("java -jar %s" % os.path.join(deps_env_vars["TRACKMAN_BIN_PATH"][0], "trackman.jar"))
-                result["startTrackman.bat"] = os.linesep.join(script_lines)
+                flavor = "cmd"
+                environment_filename = "environment{}.{}.env".format(self.suffix, flavor)
+                ret = list(self._format_values(flavor, self.env.items()))
+                modified_vars = [name for name, _, existing in ret if existing]
+                new_vars = [name for name, _, existing in ret if not existing]
 
-                script_lines, _, _ = self._ps1_lines()
-                for name, activate, deactivate in self._format_values("cmd", trackman_items.items()):
-                    script_lines.append("SET %s=%s" % (name, activate))
-                script_lines.append("java -jar %s" % os.path.join(deps_env_vars["TRACKMAN_BIN_PATH"][0], "trackman.jar"))
-                result["startTrackman.ps1"] = os.linesep.join(script_lines)
+                environment_filepath = os.path.abspath(os.path.join(self.output_path, environment_filename))
+                result["startTrackman.bat"] = cmd_trackman_script_tpl.render(environment_file=environment_filepath,
+                                                       modified_vars=modified_vars, new_vars=new_vars,
+                                                       jar_path=os.path.join(deps_env_vars["TRACKMAN_BIN_PATH"][0], "trackman.jar"),
+                                                       venv_name=self.venv_name)
 
             if os_info.is_posix:
-                script_lines, _, _ = self._sh_lines()
-                for name, activate, deactivate in self._format_values("sh", trackman_items.items()):
-                    script_lines.append("%s=%s" % (name, activate))
-                    script_lines.append("export %s" % name)
-                script_lines.append("java -jar %s" % os.path.join(deps_env_vars["TRACKMAN_BIN_PATH"][0], "trackman.jar"))
-                result["startTrackman.sh"] = os.linesep.join(script_lines)
+                flavor = "sh"
+                environment_filename = "environment{}.{}.env".format(self.suffix, flavor)
+                ret = list(self._format_values(flavor, self.env.items()))
+                modified_vars = [name for name, _, existing in ret if existing]
+                new_vars = [name for name, _, existing in ret if not existing]
 
+                environment_filepath = os.path.abspath(os.path.join(self.output_path, environment_filename))
+                result["startTrackman.sh"] = sh_trackman_script_tpl.render(environment_file=environment_filepath,
+                                                       modified_vars=modified_vars, new_vars=new_vars,
+                                                       jar_path=os.path.join(deps_env_vars["TRACKMAN_BIN_PATH"][0], "trackman.jar"),
+                                                       venv_name=self.venv_name)
         return result
 
     @property
     def content(self):
+
+        if os_info.is_windows and not os_info.is_posix:
+            self.env['UBITRACK_COMPONENTS_PATH'] = os.path.join(".", "bin", "ubitrack")
+        else:
+            self.env['UBITRACK_COMPONENTS_PATH'] = os.path.join(".", "lib", "ubitrack")
+
+        deps_env_vars = self.conanfile.deps_env_info.vars
+        for key, v in deps_env_vars.items():
+            if key == "TRACKMAN_BIN_PATH":
+              self.env["CLASSPATH"] = v[0]
+
         ret = super(ubitrack_virtualenv_generator, self).content
         return self._add_trackman_files(ret)
 
